f1adc79d203c26a0a6278d029bc1886752c4b880
Vishal <vishal2004500@gmail.com>
2026-01-03 12:55:24 +0530

    Add jobs app and category model

---
+++ jobs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    icon = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
