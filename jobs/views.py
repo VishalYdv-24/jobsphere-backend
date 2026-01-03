e9c893c3db04980e45b92fbf9e89609865171182
Vishal <vishal2004500@gmail.com>
2026-01-03 20:51:41 +0530

    Add GET /api/categories/ API

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from .serializers import CategorySerializer
+from rest_framework.response import Response
+from .models import Category,Job
 
 # Create your views here.
+class CategoryListView(APIView):
+
+    def get(self,request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
